e432353b4904ea7d0f11abb260fe2b4ea4b4ba4d
17639638825 <562985990@qq.com>
2023-08-02 21:22:28 +0800

    ddd

--- .umirc.ts
+++ .umirc.ts
@@ -3,7 +3,7 @@ const repo = 'mysite';
 export default defineConfig({
   title: 'Site Name',
   mode: 'site',
-  // more config: https://d.umijs.org/config
+  // more config: https://d.umijs.org/config,这个也修改了呀
   base: '/mysite/',
   publicPath: '/mysite/',
   exportStatic: {},

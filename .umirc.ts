91908abcc677a7544604b3cb0d6e154a866d07de
17639638825 <562985990@qq.com>
2023-08-02 20:59:44 +0800

    dgd

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   title: 'Site Name',
   mode: 'site',
   // more config: https://d.umijs.org/config
-  base: 'mysite/',
-  publicPath: 'mysite/',
+  base: '/',
+  publicPath: '/',
   exportStatic: {},
 });

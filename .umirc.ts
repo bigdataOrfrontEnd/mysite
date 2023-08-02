30a57945be5f4b93178a9f9f27bd7787d50a22c3
17639638825 <562985990@qq.com>
2023-08-02 18:22:06 +0800

    jj

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   title: 'Site Name',
   mode: 'site',
   // more config: https://d.umijs.org/config
-  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  base: `${repo}`,
+  publicPath: `${repo}`,
   exportStatic: {},
 });

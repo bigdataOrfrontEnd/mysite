2fd0a78cafa8644f569e69279e5a8fd962196cf4
17639638825 <562985990@qq.com>
2023-08-02 18:19:07 +0800

    tijiao

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   title: 'Site Name',
   mode: 'site',
   // more config: https://d.umijs.org/config
-  base: '/<repo>/',
-  publicPath: '/<repo>/',
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   exportStatic: {},
 });
